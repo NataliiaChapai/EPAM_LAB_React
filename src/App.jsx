0b4f8aae3127b6cc779ab48e967443e851dfacff
Nataliia Chapai <natalya.chapay@gmail.com>
2022-12-18 17:28:21 +0200

    adds Logo component

---
+++ src/App.jsx
@@ -0,0 +1,6 @@
+import Logo from './components/Header/components/Logo/Logo';
+
+function App() {
+	return <Logo></Logo>;
+}
+export default App;
---
+++ src/components/Header/components/Logo/Logo.jsx
@@ -0,0 +1,6 @@
+import logo from './logo.png';
+
+function Logo() {
+	return <img src={logo} alt='logo' />;
+}
+export default Logo;
