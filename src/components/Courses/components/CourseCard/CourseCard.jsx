49e4b8d201cfd3b59da16bb9d577429c989e8d08
Nataliia Chapai <natalya.chapay@gmail.com>
2022-12-28 01:15:06 +0200

    adds functionality to the 'Show course' button

--- src/components/Courses/components/CourseCard/CourseCard.jsx
+++ src/components/Courses/components/CourseCard/CourseCard.jsx
@@ -14,32 +14,42 @@ import {
 	StyledSpan,
 	StyledBtn,
 } from './CourseCard.styled';
+import { useNavigate } from 'react-router';
 
-export const CourseCard = ({ courseItem }) => (
-	<StyledCourseCard>
-		<StyledLeftDiv>
-			<StyledTitle>{courseItem.title}</StyledTitle>
-			<p>{courseItem.description}</p>
-		</StyledLeftDiv>
-		<StyledRightDiv>
-			<StyledText>
-				<StyledSpan>Authors: </StyledSpan>
-				{renderAuthors(courseItem.authors)}
-			</StyledText>
-			<StyledText>
-				<StyledSpan>Duration: </StyledSpan>
-				{pipeDuration(courseItem.duration)}
-			</StyledText>
-			<StyledText>
-				<StyledSpan>Created: </StyledSpan>
-				{dateGenerator(courseItem.creationDate, 'dd.mm.yyyy')}
-			</StyledText>
-			<StyledBtn>
-				<Button buttonText='Show course'></Button>
-			</StyledBtn>
-		</StyledRightDiv>
-	</StyledCourseCard>
-);
+export const CourseCard = ({ courseItem }) => {
+	const navigate = useNavigate();
+
+	const handleClick = (id) => navigate(`/courses/${id}`);
+
+	return (
+		<StyledCourseCard>
+			<StyledLeftDiv>
+				<StyledTitle>{courseItem.title}</StyledTitle>
+				<p>{courseItem.description}</p>
+			</StyledLeftDiv>
+			<StyledRightDiv>
+				<StyledText>
+					<StyledSpan>Authors: </StyledSpan>
+					{renderAuthors(courseItem.authors)}
+				</StyledText>
+				<StyledText>
+					<StyledSpan>Duration: </StyledSpan>
+					{pipeDuration(courseItem.duration)}
+				</StyledText>
+				<StyledText>
+					<StyledSpan>Created: </StyledSpan>
+					{dateGenerator(courseItem.creationDate, 'dd.mm.yyyy')}
+				</StyledText>
+				<StyledBtn>
+					<Button
+						buttonText='Show course'
+						onClick={() => handleClick(courseItem.id)}
+					></Button>
+				</StyledBtn>
+			</StyledRightDiv>
+		</StyledCourseCard>
+	);
+};
 
 CourseCard.propTypes = {
 	courseItem: PropTypes.shape({
